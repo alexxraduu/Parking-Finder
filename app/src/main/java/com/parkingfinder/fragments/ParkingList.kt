fe68877538542920fe30380f41f2c830b0c7c3af
Radu Alexandru <alexx15raduu@gmail.com>
2021-06-19 20:27:36 +0300

    Logout functionality.

--- app/src/main/java/com/parkingfinder/fragments/ParkingList.kt
+++ app/src/main/java/com/parkingfinder/fragments/ParkingList.kt
@@ -1,21 +1,26 @@
 package com.parkingfinder.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.parkingfinder.R
+import com.parkingfinder.activities.MainActivity
+import com.parkingfinder.activities.SecondActivity
 import com.parkingfinder.interfaces.ActivityFragmentCommunication
 
 
 class ParkingList : Fragment() {
     var activityFragmentCommunication: ActivityFragmentCommunication? = null
-    var tv: TextView? = null
+    var btn: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -25,7 +30,7 @@ class ParkingList : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_parking_list, container, false)
-        tv=view.findViewById(R.id.test)
+        btn=view.findViewById(R.id.btn_logout)
         return view
     }
 
@@ -35,8 +40,26 @@ class ParkingList : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        tv?.text=Firebase.auth.currentUser?.email.toString()
+        btn?.setOnClickListener {
+            logout()
+        }
     }
+
+    private fun logout() {
+        Firebase.auth.signOut()
+        if(Firebase.auth.currentUser?.email.isNullOrEmpty()){
+            Toast.makeText(
+                context,
+                "Logged out",
+                Toast.LENGTH_LONG
+            )
+                .show()
+        }
+        val intent = Intent(context, MainActivity::class.java)
+        activity?.startActivity(intent)
+        activity?.finish()
+    }
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is ActivityFragmentCommunication) {
