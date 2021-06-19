5da255e15787b2666adba006f134d912d3d37a37
florin <florinstroe2020@gmail.com>
2021-06-19 14:27:25 +0300

    Login & Register page UI.

--- app/src/main/java/com/parkingfinder/MainActivity.kt
+++ app/src/main/java/com/parkingfinder/MainActivity.kt
@@ -2,21 +2,24 @@ package com.parkingfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.GeoPoint
-import com.google.firebase.ktx.Firebase
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.parkingfinder.fragments.LoginRegister
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addLoginRegisterFragment()
     }
 
-
+    fun addLoginRegisterFragment() {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val tag: String = LoginRegister::class.java.getName()
+        val addTransaction: FragmentTransaction = transaction.add(
+            R.id.frame_layout, LoginRegister(), tag
+        )
+        addTransaction.commit()
+    }
 }
